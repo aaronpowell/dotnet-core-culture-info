1009a10ad8c7337ce9010d3128fc8dc20bd81f9f
Aaron Powell <me@aaron-powell.com>
2016-07-29 15:54:59 +1000

    initial commit

---
+++ CultureDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var culture = new CultureInfo("fr-au");
+
+            Console.WriteLine(culture);
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM microsoft/dotnet:1.0.0-core
+
